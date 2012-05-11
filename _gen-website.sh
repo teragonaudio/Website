74bab19f63fea1ca7ae4bd3647adfd738b039e54
Nik Reiman <nik.reiman@gmail.com>
2012-05-11 15:01:14 +0200

    Remove _site before building

--- _gen-website.sh
+++ _gen-website.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/bin/rm -rf _site
 jekyll --no-server
 /bin/cp -r -v _site/* ../teragonaudio.github.com
 (cd ../teragonaudio.github.com && git add -A . && git commit -m "Sync website content" && git push)
