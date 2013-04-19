89308b447c89cc7d32c0a790e5cbd563ce754b57
Nik Reiman <nik.reiman@gmail.com>
2013-04-19 22:04:01 +0200

    Don't automatically push after generating website

--- _scripts/gen-website.sh
+++ _scripts/gen-website.sh
@@ -3,4 +3,4 @@
 jekyll --no-server
 outDir="_out"
 /bin/cp -r -v _site/* "${outDir}"
-(cd "${outDir}" && git add -A . && git commit -m "Sync website content" && git push)
+#(cd "${outDir}" && git add -A . && git commit -m "Sync website content" && git push)
